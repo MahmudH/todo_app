794bac5e23631cf5ad3d83b96ecc5c0f5ad0afa3
mahmudh <mahmudhssn246@gmail.com>
2015-07-07 15:57:14 +0100

    add devise for user authentication

--- Gemfile
+++ Gemfile
@@ -19,6 +19,8 @@ gem 'turbolinks'
 
 gem 'jbuilder', '~> 2.0'
 
+gem 'devise', '~> 3.5.1'
+
 gem 'sdoc', '~> 0.4.0', group: :doc
 
 
--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,24 +1,29 @@
 class TodosController < ApplicationController
+
 	def index
 		@todo = Todo.all
 	end
 
 	def new
-		@todo = Todo.new
+		@todo = current_user.todos.build
 	end
 
 	def create
-		@todo = Todo.new(todo_params)
+		@todo = current_user.todos.build(todo_params)
 		if @todo.save
 			redirect_to @todo
 
 		else
-			# render 'edit'
+			render 'edit'
 		end
 	end
 
 	def show
 		@todo = Todo.find(params[:id])
+	end
+
+	def edit
+		
 	end
 
 	private
